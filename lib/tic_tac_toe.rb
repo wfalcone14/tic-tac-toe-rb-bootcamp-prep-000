b7c8d3a72df38b1b21a3d07fe7c920cda1c4afe6
William Falcone <wjf0003+will@gmail.com>
2018-01-23 23:26:26 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -31,9 +31,9 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if index.between?(0,8) && position_taken?(board,index) == false
-    true 
-  end
+ def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
 end
 
 def turn(board)
