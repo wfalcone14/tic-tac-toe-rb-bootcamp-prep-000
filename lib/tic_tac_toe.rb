2ff3e0781d93b420288910063eb623b86ab475f9
William Falcone <wjf0003+will@gmail.com>
2018-01-23 23:43:32 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,7 +39,7 @@ def turn(board)
   input = gets.strip
   input_to_index(input)
   if valid_move?(board,index)
-    move(board, index, current_player(board))
+    move(board,index, current_player(board))
     display_board(board)
   else
     turn(board)
