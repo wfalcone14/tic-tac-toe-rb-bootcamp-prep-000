9956255b43537a6b0cf2a91df9aff93e3ea8cc43
William Falcone <wjf0003+will@gmail.com>
2018-01-23 23:30:30 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -30,7 +30,7 @@ def position_taken?(board,index)
   board[index].nil? || board[index] == " "
 end
 
-def valid_move?(board, index)
+def valid_move?(board,index)
   index.between?(0,8) && !position_taken?(board,index)
 end
 
